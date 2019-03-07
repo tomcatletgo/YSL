1aa103c31b93cc251dd06d4a2414aee5ff803812
Natsumi7777777 <642713403@qq.com>
2019-03-07 13:24:59 +0800

    111

--- src/main/java/com/marriage/news/service/NewsService.java
+++ src/main/java/com/marriage/news/service/NewsService.java
@@ -1,5 +1,20 @@
 package com.marriage.news.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.marriage.user.entity.ManInformation;
+
 public interface NewsService {
+	
+	Integer addNews(Map<String, Object> map);
+
+	List<ManInformation> selectNewsBypage(Map<String, Object> map);
+
+	Integer updateNewsByNewsId(Map<String, Object> map);
+
+	Integer deleteNewsById(Map<String, Object> map);
+
+	
 
 }
