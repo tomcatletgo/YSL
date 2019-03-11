67978c69083fbe8197e714b4d80d124edaeae28a
tomcatletgo <yu1156725315@sina.com>
2019-03-11 10:21:16 +0800

    33333

--- src/main/java/com/marriage/message/service/MessageService.java
+++ src/main/java/com/marriage/message/service/MessageService.java
@@ -1,5 +1,22 @@
 package com.marriage.message.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.marriage.message.entity.MessageEntity;
+
 public interface MessageService {
+	
+	public <T> List<T> findMessageList(Map<String,Object>Map);
+	
+	public MessageEntity findMessageById(Map<String,Object>Map);
+	
+	public int delSingleMessage(Map<String,Object>Map);
+	
+	public int delMoreMessage(Map<String,Object>Map);
+	
+	public <T> List<T> updateMessageList(Map<String,Object>Map);
 	
 }
